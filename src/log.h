03dac3e90c2d9e6d9e50d3d13055401b3ec8a4ae
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-01-18 15:22:02 +0530

    add log.h

---
+++ src/log.h
@@ -0,0 +1,46 @@
+#ifndef LOG_H
+#define LOG_H
+/* SPDX-FileCopyrightText: 2021 git-bruh
+ * SPDX-License-Identifier: GPL-3.0-or-later */
+#include <assert.h>
+#include <stdio.h>
+#include <time.h>
+
+enum log_level { LOG_MESSAGE, LOG_WARN, LOG_ERROR, LOG_MAX };
+
+__attribute__((unused)) static inline void
+log_level_and_time(enum log_level level) {
+	assert(level < LOG_MAX);
+
+	enum { bufsz = 8 + 1 };
+
+	const char level_to_ch[LOG_MAX] = {
+	  [LOG_MESSAGE] = 'M',
+	  [LOG_WARN] = 'W',
+	  [LOG_ERROR] = 'E',
+	};
+
+	char buf[bufsz] = {0};
+	struct tm tm = {0};
+
+	if ((localtime_r(&(time_t) {time(NULL)}, &tm))) {
+		buf[strftime(buf, sizeof(buf), "%I:%M:%S", &tm)] = '\0';
+	}
+
+	fprintf(stderr, "[%s] %c: ", buf, level_to_ch[level]);
+}
+
+#define LOG(level, ...)                                                        \
+	do {                                                                       \
+		_Static_assert(level < LOG_MAX, "Invalid log level!");                 \
+		/* https://stackoverflow.com/a/2124433 */                              \
+		_Static_assert(                                                        \
+		  (sizeof((const char *[]) {"", ##__VA_ARGS__}) / sizeof(char *) - 1)  \
+			> 0,                                                               \
+		  "Missing log message!");                                             \
+		log_level_and_time(level);                                             \
+		/* Hide expected expression error caught by static assertion. */       \
+		fprintf(stderr, "" __VA_ARGS__);                                       \
+		fputc('\n', stderr);                                                   \
+	} while (0)
+#endif /* !LOG_H */
