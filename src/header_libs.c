2bd68e6a95526d932efc03c9f3164652392f2db4
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-01-23 18:37:37 +0530

    shift termbox-widgets submodule from shithub

--- src/header_libs.c
+++ src/header_libs.c
@@ -1,6 +1,5 @@
-#define STB_DS_IMPLEMENTATION
-#include "stb_ds.h"
-#undef STB_DS_IMPLEMENTATION
 #define TB_IMPL
-#include "termbox.h"
-#undef TB_IMPL
+#define WIDGETS_IMPL
+#define STB_DS_IMPLEMENTATION
+/* Includes termbox.h and stb_ds.h */
+#include "widgets.h"
